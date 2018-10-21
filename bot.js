eac655334d3de530a50602edc46348541bc53ea5
7oDA22 <34072752+7oDA22@users.noreply.github.com>
2018-10-21 12:42:31 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
- client.user.setActivity("~help ~inv",{type: 'watching'})
+ client.user.setActivity("Top",{type: 'watching'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
